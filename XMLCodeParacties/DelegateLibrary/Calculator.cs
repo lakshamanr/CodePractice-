a678cd79fa782c45e4fca4afd762f5c01fe24155
Lakshaman Rokade <lakshaman.rokade@sagitec.com>
2023-10-15 11:00:07 -0500

    Delegate first Demo

---
+++ XMLCodeParacties/DelegateLibrary/Calculator.cs
@@ -0,0 +1,19 @@
+﻿namespace DelegateLib
+{
+    public delegate int MathsAddDelegate(int number1, int number2);
+
+    public class Calculator
+    {
+        private readonly MathsAddDelegate _calculatorDelegate;
+
+        public Calculator(MathsAddDelegate delegateReference)
+        {
+            _calculatorDelegate = delegateReference;
+        }
+
+        public int Add(int aintNumber1, int aintNumber2)
+        {
+            return _calculatorDelegate.Invoke(aintNumber1, aintNumber2);
+        }
+    }
+}
---
+++ XMLCodeParacties/TestDelegates/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DelegateLib;
+namespace TestDelegates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            MathAddition Addition = new MathAddition();
+            MathsAddDelegate @delegate = Addition.MathAdd;
+            Calculator calculator = new Calculator(@delegate);
+            Console.WriteLine($"Addition of the Two Numbers are :{calculator.Add(10, 20)}"); 
+        }
+    }
+
+    public class MathAddition
+    {
+        public int MathAdd(int aintNumber1 , int aintNumber2)
+        {
+            return aintNumber1 + aintNumber2;
+        }
+    }
+}

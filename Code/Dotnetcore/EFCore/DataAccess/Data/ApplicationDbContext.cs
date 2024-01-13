f9774ad80dbcef6a9725f16adf32b7c02ba1a65b
Lakshaman Rokade <lakshaman.rokade@sagitec.com>
2024-01-13 13:05:23 -0600

    inital Project setup

---
+++ Code/Dotnetcore/EFCore/DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataModel.ModelClass;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+    }
+}
